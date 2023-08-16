f6dfb5aba63791b041f077cdb17aaf5cf72266ed
Kevin <kevinnwjchoi@gmail.com>
2023-08-15 21:58:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify({name, email}),
+};
+
+return fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const { id } = data;
+    const idElement = document.createElement("p");
+    idElement.textContent = `New ID: ${id}`;
+    document.body.appendChild(idElement);
+  })
+  .catch(function (error) {
+    const errorElement = document.createElement("p");
+    errorElement.textContent = `Error: ${error.message}`;
+    document.body.appendChild(errorElement);
+  });
+
+
+}
+submitData();
+
